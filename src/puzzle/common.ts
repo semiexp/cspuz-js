3f9f404b3eda5a24e35dc577af5aec942c868387
semiexp <linthya.v@gmail.com>
2020-09-27 18:04:15 +0900

    Add Yajilin solver

--- src/puzzle/common.ts
+++ src/puzzle/common.ts
@@ -1,3 +1,12 @@
 export const Undecided: number = -1;
 export const BlackCell: number = -2;
 export const WhiteCell: number = -3;
+
+export enum Direction {
+    Up,
+    Left,
+    Right,
+    Down
+}
+
+export type NumberWithDirection = { direction: Direction, value: number };
--- src/puzzle/puzzle.ts
+++ src/puzzle/puzzle.ts
@@ -1,6 +1,7 @@
 export * from './common'
 export {solveNurikabeAsync} from './nurikabe'
 export {solveSlitherlinkAsync} from './slitherlink'
+export {solveYajilinAsync} from './yajilin'
 
 export enum Edge {
     Undecided,
